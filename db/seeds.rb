c522ccbd36e37b84adc64bfff9e550148a77592f
David Espino <davidespinog@gmail.com>
2023-07-09 22:13:07 -0600

    Add seed and greeting model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.destroy_all
+
+Greeting.create name: 'Hi'
+Greeting.create name: 'Good morning'
+Greeting.create name: 'Hello'
+Greeting.create name: 'Good night'
+Greeting.create name: 'Good afternoon'
+Greeting.create name: 'Hi!'
+Greeting.create name: 'Hi! Dear user'
+Greeting.create name: 'Hi! Welcome'
+Greeting.create name: 'Welcome, user'
